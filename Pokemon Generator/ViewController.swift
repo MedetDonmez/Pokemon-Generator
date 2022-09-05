36e882e7c618a4ccb9718588a958abca1cb648d6
Medet Dönmez <medetdonmez97@gmail.com>
2022-09-05 12:14:54 +0300

    Initial Commit

---
+++ Pokemon Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon Generator
+//
+//  Created by Medet Dönmez on 5.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
